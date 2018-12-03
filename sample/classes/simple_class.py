3fa14042e837c3fe71ebdcad487983d293916941
Reus <l.reus@wakeonweb.com>
2018-12-03 16:53:12 +0100

    New simple class for simple tests...

---
+++ sample/classes/simple_class.py
@@ -0,0 +1,10 @@
+class SimpleClass:
+    """
+        A simple class to be tested by features localized in tests/features
+    """
+    def hello(self) -> str:
+        """
+        :return: a simple string
+        :rtype: str
+        """
+        return 'wave'
