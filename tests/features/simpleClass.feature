1d5b8babbe48c22601a20b9bd4e91eefa8e89c40
Reus <l.reus@wakeonweb.com>
2018-12-06 14:18:10 +0100

    Add a way for genericly testing inheritance with behave

--- tests/features/simpleClass.feature
+++ tests/features/simpleClass.feature
@@ -1,7 +1,11 @@
 Feature: Test a simple class out of the test directory
 
-  @wip
   Scenario: Say hello and receive a wave
     Given I have a simple class
     When I say hello
     Then the class should wave
+
+  @wip
+  Scenario: Assert about inheritance of a class
+    Given I have a simple subclass
+    Then the subclass shall inherit from "SimpleClass"
--- tests/features/steps/simple_class_test.py
+++ tests/features/steps/simple_class_test.py
@@ -1,5 +1,18 @@
 from sample.classes.simple_class import SimpleClass
+from sample.classes.subclasses.simple_subclass import SimpleSubClass
 from behave import *
+
+
+def class_mapping():
+    """
+
+    :return:
+    :rtype: dict
+    """
+    return {
+        'SimpleClass': SimpleClass,
+        'SimpleSubClass': SimpleSubClass,
+    }
 
 
 @Given('I have a simple class')
@@ -15,3 +28,22 @@ def i_say_hello(context):
 @Then('the class should wave')
 def class_should_wave(context):
     assert context.response == 'wave'
+
+
+@Given('I have a simple subclass')
+def i_have_a_simple_subclass(context):
+    context.Class = {'subclass': SimpleSubClass()}
+
+
+@Then('the subclass shall inherit from "{class_name}"')
+def the_subclass_shall_inherit_from(context, class_name):
+    try:
+        mapped_class = class_mapping()[class_name]
+    except KeyError:
+        print('available class to test are', [k for k in class_mapping()])
+        raise AssertionError
+
+    assert issubclass(
+        type(context.Class['subclass']),
+        mapped_class
+    )
