f02b17d78ad264f59517360e4ad0267fa9280a5b
Reus <l.reus@wakeonweb.com>
2018-12-09 19:31:22 +0100

    Implement autoloaded script in behave_pgsl to create kong database

--- conf/pgsql.init/init-kong-db.sh
+++ conf/pgsql.init/init-kong-db.sh
@@ -4,5 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER behave_kong WITH PASSWORD 'kong';
     CREATE DATABASE behave_kong_db;
-    GRANT ALL PRVILEGES ON DATABASE behave_kong_db TO behave_kong;
+    GRANT ALL PRIVILEGES ON DATABASE behave_kong_db TO behave_kong;
 EOSQL
