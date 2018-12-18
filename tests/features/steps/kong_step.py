5cb336fef53b73c2bee8cf578528331b2085d74e
Reus <l.reus@wakeonweb.com>
2018-12-18 09:56:03 +0100

    Rework script line length to comply PEP-8 79 character recommendation

--- tests/features/steps/kong_step.py
+++ tests/features/steps/kong_step.py
@@ -9,7 +9,10 @@ import requests
 import json
 
 
-def build_unexpected_code_message(expected_code: List[int], received_code: int) -> str:
+def build_unexpected_code_message(
+        expected_code: List[int],
+        received_code: int
+) -> str:
     """build_unexpected_code_message(expected_code: int, received_code: int)
 
     :param expected_code: The expected http code
@@ -19,8 +22,10 @@ def build_unexpected_code_message(expected_code: List[int], received_code: int) 
 
     :rtype: str
     """
-    return 'Unexpected status code: {code} while expecting {expectation} '\
-        .format(code=received_code, expectation=' or '.join(expected_code))
+    return 'Unexpected status code: {got} while expecting {expectation} '\
+        .format(
+            got=received_code,
+            expectation=' or '.join(str(code) for code in expected_code))
 
 
 def context_has_valid_response(context: Context = None) -> bool:
@@ -41,7 +46,8 @@ def context_has_valid_response(context: Context = None) -> bool:
         'No response attribute set in context manager'
 
     assert isinstance(context.response, Response), \
-        'Attribute response in context manager contains {class_name}'.format(class_name=context.response.__class__)
+        'Attribute response in context manager contains {class_name}'\
+        .format(class_name=context.response.__class__)
 
     return True
 
@@ -66,15 +72,20 @@ def kong_is_accessible(context: Context, url: str = '') -> None:
     :raise: AssertionError
     """
     response = requests.get(url)
-    api_error = 'No api answered at {url} with a status code 200'.format(url=url)
+    api_error = 'No api answered at {url} with a status code 200'\
+        .format(url=url)
 
     assert isinstance(response, requests.Response), api_error
-    assert response.status_code == 200, build_unexpected_code_message([200], response.status_code)
+    assert response.status_code == 200, build_unexpected_code_message(
+        [200],
+        response.status_code)
 
     response_content = json.loads(response.content)
 
-    assert ('tagline' in response_content and response_content['tagline'] == 'Welcome to kong'), \
-        'Unable to find tagline "Welcome to kong" in response json content'
+    assert (
+            'tagline' in response_content
+            and response_content['tagline'] == 'Welcome to kong'
+    ), 'Unable to find tagline "Welcome to kong" in response json content'
 
     context.kong_url = url
 
@@ -85,9 +96,11 @@ def there_is_no_service(context: Context, service_name: str = '') -> None:
 
     Behave background step: Given there is no service named "{service_name}".
     Request the provided service name to kong api.
-    If the service exists call step 'Given i delete the service' and perform a new request.
+    If the service exists call step 'Given i delete the service' and perform a
+    new request.
     Expects the response status code 404 Not found.
-    Uses context.kong_url string initialized in kong_is_accessible background test.
+    Uses context.kong_url string initialized in kong_is_accessible background
+    test.
 
     :param context: Behave context object
     :type context: behave.runner.Context
@@ -112,17 +125,24 @@ def there_is_no_service(context: Context, service_name: str = '') -> None:
         '''.format(service_name=service_name))
         response = requests.get(url)
 
-    assert response.status_code == 404, build_unexpected_code_message([404], response.status_code)
+    assert response.status_code == 404, build_unexpected_code_message(
+        [404],
+        response.status_code)
 
 
 @When('I create a service named "{service_name}" pointing to "{service_url}"')
-def i_create_service(context: Context, service_name: str = '', service_url: str = '') -> None:
+def i_create_service(
+        context: Context,
+        service_name: str = '',
+        service_url: str = ''
+) -> None:
     """i_create_service
 
     Behave background step: When I create a service named "{service_name}".
     Request kong to create a service with the provided name.
     Expects the response to be a 201 Created.
-    Uses context.kong_url string initialized in kong_is_accessible background test.
+    Uses context.kong_url string initialized in kong_is_accessible background
+    test.
 
     :param context: Behave context object
     :type context: behave.runner.Context
@@ -146,7 +166,9 @@ def i_create_service(context: Context, service_name: str = '', service_url: str 
     }
     response = requests.post(url, post_data)
 
-    assert response.status_code == 201, build_unexpected_code_message([201], response.status_code)
+    assert response.status_code == 201, build_unexpected_code_message(
+        [201],
+        response.status_code)
 
 
 @Given('I delete the service "{service_name}"')
@@ -156,7 +178,8 @@ def i_delete_service(context: Context, service_name: str = '') -> None:
     Behave background step: Given I delete the service "{service_name}".
     Request kong to delete a service named with the provided name.
     Expects the response to be a 204 No content.
-    Uses context.kong_url string initialized in kong_is_accessible background test.
+    Uses context.kong_url string initialized in kong_is_accessible background
+    test.
 
     :param context: Behave context object
     :type context: behave.runner.Context
@@ -173,7 +196,9 @@ def i_delete_service(context: Context, service_name: str = '') -> None:
     url = ''.join((context.kong_url, '/services/', service_name, '/'))
     response = requests.delete(url)
 
-    assert response.status_code == 204, build_unexpected_code_message([204], response.status_code)
+    assert response.status_code == 204, build_unexpected_code_message(
+        [204],
+        response.status_code)
 
 
 @When('I request kong for service "{service_name}"')
@@ -183,7 +208,8 @@ def i_request_service(context: Context, service_name: str = '') -> None:
     Behave background step: When I request kong for service "{service_name}".
     Request kong for a service named with provided parameter
     Store response in context.response attribute.
-    Uses context.kong_url string initialized in kong_is_accessible background test.
+    Uses context.kong_url string initialized in kong_is_accessible background
+    test.
 
     :param context: Behave context object
     :type context: behave.runner.Context
@@ -222,7 +248,8 @@ def request_code_equals(context: Context, code: int) -> None:
     :raise: AssertionError
     """
     if context_has_valid_response(context):
-        assert context.response.status_code == code, build_unexpected_code_message([200], context.response.status_code)
+        assert context.response.status_code == code, \
+            build_unexpected_code_message([200], context.response.status_code)
 
 
 @Then('the response content should contain JSON')
@@ -231,7 +258,8 @@ def is_response_json(context: Context):
 
     Behave background step: Then the response content should contain JSON
     Uses context_has_valid_response to validate context response.
-    Decode as Json the context.response.content and loads it context.json_response.
+    Decode as Json the context.response.content and saves it in
+    context.json_response.
 
     :param context: Behave context object
     :type context: Context
@@ -248,9 +276,11 @@ def is_response_json(context: Context):
 def response_contain_key_value(context: Context) -> None:
     """response_contain_key_value
 
-    Behave background step: Then the response should have the matching key-values pairs.
+    Behave background step: Then the response should have the matching
+    key-values pairs.
     Validate that the context manager has an attribute json_response.
-    Check if each key/value pair from the provided context table appears in context.json_response.
+    Check if each key/value pair from the provided context table appears in
+    context.json_response.
 
     :param context: Behave context object
     :type context: Context
@@ -260,23 +290,29 @@ def response_contain_key_value(context: Context) -> None:
     :raise: AssertionError
     """
     context.table: Table
-    assert hasattr(context, 'json_response') and context.json_response is not None, \
+    assert hasattr(context, 'json_response') \
+        and context.json_response is not None, \
         'No json_response attribute in context manager'
 
     assert isinstance(context.json_response, dict), \
-        'context.json_response is not a dict but a {class_name}'.format(class_name=context.json_response.__class__)
+        'context.json_response is not a dict but a {class_name}'\
+        .format(class_name=context.json_response.__class__)
 
-    assert 'key' in context.table.headings, 'Please use the word "key" as first column header for this step'
-    assert 'value' in context.table.headings, 'Please use the word "value" as first column header for this step'
+    assert 'key' in context.table.headings,\
+        'Please use the word "key" as first column header for this step'
+
+    assert 'value' in context.table.headings,\
+        'Please use the word "value" as first column header for this step'
 
     for row in context.table.rows:
-        assert row['key'] in context.json_response, 'Unable to find key {key} in json response'.format(key=row['key'])
+        assert row['key'] in context.json_response,\
+            'Unable to find key {key} in json response'.format(key=row['key'])
 
         incoming_value = context.json_response[row['key']]
         assert row['value'] == str(incoming_value), \
             '''
-            Unexpected value for json_response["{key}"] got {unexpected} <{unexpected_class}> 
-            while expecting {value} <{class_name}>
+            Unexpected value for json_response["{key}"] got {unexpected} 
+            <{unexpected_class}> while expecting {value} <{class_name}>
             '''.format(
                 key=row['key'],
                 unexpected=context.json_response[row['key']],
@@ -287,12 +323,19 @@ def response_contain_key_value(context: Context) -> None:
 
 
 @When('I create a consumer named "{username}" with id "{custom_id}"')
-def i_create_consumer(context: Context, username: str = '', custom_id: str = '') -> None:
+def i_create_consumer(
+        context: Context,
+        username: str = '',
+        custom_id: str = ''
+) -> None:
     """there_is_a_consumer_identified_by
 
-        Behave background step: When I create a consumer named "{username}" with id "{custom_id}".
-        Creates a consumer
-        Uses context.kong_url string initialized in kong_is_accessible background test.
+        Behave background step: When I create a consumer named "{username}"
+        with id "{custom_id}".
+        Creates a consumer.
+
+        Uses context.kong_url string initialized in kong_is_accessible
+        background test.
 
         :param context: Behave context object
         :type context: Context
@@ -316,7 +359,9 @@ def i_create_consumer(context: Context, username: str = '', custom_id: str = '')
     url = ''.join((context.kong_url, '/consumers/'))
     response = requests.post(url, user)
 
-    assert response.status_code == 201, build_unexpected_code_message([201], response.status_code)
+    assert response.status_code == 201, build_unexpected_code_message(
+        [201],
+        response.status_code)
 
 
 @Given('there is a consumer identified by')
@@ -325,10 +370,13 @@ def there_is_a_consumer_identified_by(context: Context) -> None:
 
     Behave background step: Given there is a consumer identified by.
     Ensure a consumer exists in kong service.
-    Attempt to get the consumer with provided credentials in context.Model.Table.
+    Attempt to get the consumer with provided credentials in
+    context.Model.Table.
+
     Execute consumer creation step if the consumer is not found.
     Perform a second Call to store the consumer id.
-    Uses context.kong_url string initialized in kong_is_accessible background test.
+    Uses context.kong_url string initialized in kong_is_accessible background
+    test.
 
     :param context: Behave context object
     :type context: Context
@@ -340,8 +388,11 @@ def there_is_a_consumer_identified_by(context: Context) -> None:
     context.table: Table
     context.kong_url: str
 
-    assert 'username' in context.table.headings, 'Please use the word "username" as first column header for this step'
-    assert 'custom_id' in context.table.headings, 'Please use the word "custom_id" as first column header for this step'
+    assert 'username' in context.table.headings,\
+        'Please use the word "username" as first column header for this step'
+
+    assert 'custom_id' in context.table.headings,\
+        'Please use the word "custom_id" as first column header for this step'
 
     user = {
         'username': context.table.rows[0]['username'],
@@ -352,7 +403,9 @@ def there_is_a_consumer_identified_by(context: Context) -> None:
 
     response = requests.get(url)
 
-    assert response.status_code in [404, 200], build_unexpected_code_message([404, 200], response.status_code)
+    assert response.status_code in [404, 200], build_unexpected_code_message(
+        [404, 200],
+        response.status_code)
 
     if response.status_code == 404:
         context.execute_steps(u'''
@@ -361,7 +414,6 @@ def there_is_a_consumer_identified_by(context: Context) -> None:
         )
         response = requests.get(url)
 
-    assert response.status_code == 200, build_unexpected_code_message([200], response.status_code)
-
-
-
+    assert response.status_code == 200, build_unexpected_code_message(
+        [200],
+        response.status_code)
